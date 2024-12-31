a75eccb04fa26976ba4b7c24cdb58e0d80cfe5f2
Vancy bukama <vancybukama@gmail.com>
2025-01-01 00:44:08 +0200

    My Commit

---
+++ index.js
@@ -0,0 +1,33 @@
+$(function () {
+    $(".menu").click(function () {
+        $(".list-form").fadeToggle(3000);
+    })
+})
+
+// JQUERY ANIMATION
+// $(function () {
+//     $(".btn3").click(function () {
+//         $(".btn3").animate({opacity:0.5})
+//     })
+// })
+
+// import express from "express";
+// const app = express();
+// const port = 3000;
+// app.listen(port,()=>{
+//     console.log("Server running on "+port);
+// })
+
+// VANILLA JAVASCRIPT
+
+// const menu = document.querySelector(".menu");
+// menu.addEventListener('click',()=>{
+//     const list = document.querySelector(".list-form");
+//     list.classList.toggle("class");
+// })
+
+
+$(function () {
+    $(".hero-section").slideDown(3000) 
+})
+
